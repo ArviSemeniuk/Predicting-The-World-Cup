eb63f7ff02889779e9e74d3c00fb7712e54a8f40
Arvaidas Semeniuk <25202328@students.lincoln.ac.uk>
2022-03-15 13:31:15 +0000

    Encoded our data using OneHotEncoding and OrdinalEncoding

--- worldcupml.py
+++ worldcupml.py
@@ -1,20 +1,33 @@
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import LabelEncoder
+import sklearn
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
 
-cols = ["Year", "Location", "Average Yearly Temperature (Celsius)", "TeamA", "TeamB", "Round", "Full-time score TeamA", "Full-time score TeamB"]
-world_cup_data = pd.read_csv("worldcupdata.csv", encoding="cp1252", usecols=cols) #Loading the data from the csv file. 
+#cols = ["Year", "Location", "Average Yearly Temperature (Celsius)", "TeamA", "TeamB", "Round", "Full-time score TeamA", "Full-time score TeamB"]
+world_cup_data = pd.read_csv("worldcupdata.csv", encoding="latin-1") #Loading the data from the csv file. 
 
-labelencoder = LabelEncoder()
-world_cup_data["Location"] = labelencoder.fit_transform(world_cup_data["Location"])
-world_cup_data["TeamA"] = labelencoder.fit_transform(world_cup_data["TeamA"])
-world_cup_data["TeamB"] = labelencoder.fit_transform(world_cup_data["TeamB"])
-#world_cup_data["Form of TeamA"] = labelencoder.fit_transform(world_cup_data["Form of TeamA"])
-#world_cup_data["Form of TeamB"] = labelencoder.fit_transform(world_cup_data["Form of TeamB"])
+#Transform categorical data into numerical data using ohe for nominal variables and oe for ordinal variables
+ohe = OneHotEncoder()
+encodedNominal = ohe.fit_transform(world_cup_data[["Location", "TeamA", "TeamB"]]).toarray()
+print(encodedNominal)
 
-hotencoder = OneHotEncoder(handle_unknown="ignore")
-encoded_df = pd.DataFrame(hotencoder.fit_transform(world_cup_data[["Location"]]).toarray())
+oe = OrdinalEncoder(categories=[["Good", "Neutral", "Bad"], ["Good", "Neutral", "Bad"]])
+encodedOrdinal = oe.fit_transform(world_cup_data[["Form of TeamA", "Form of TeamB"]])
+print(oe.categories_)
+print(encodedOrdinal)
 
-world_cup_data = world_cup_data.join(encoded_df)
-world_cup_data
+#Scale numerical data to a standard range
+X = world_cup_data[["Year", "Average Yearly Temperature (Celsius)", "Round", "Full-time score TeamA", "Full-time score TeamB"]]
+scale = StandardScaler()
+scaledX = scale.fit_transform(X)
+
+print(scaledX)
+
+#Multioutput Regression
+
+#print(sklearn.__version__)
+
+model = LinearRegression()
